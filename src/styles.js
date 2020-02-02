11b427d42e174333ed76663b6349f16815053162
LopezDP <davidplopez@live.com>
2020-02-02 15:49:37 -0500

    feat: Adding responsiveness to component to display on smaller iOS devices.

--- src/styles.js
+++ src/styles.js
@@ -1,119 +1,119 @@
 export const styles = {
   githubFeed: {
-    'position': 'relative',
-    'display': 'table',
-    'border': '1px solid #ddd',
-    'height': '100%',
-    'minHeight': '200px',
-    'width': '100%',
-    'minWidth': '500px',
-    'fontFamily': '-apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", Arial, sans-serif',
-    'fontSize': '1rem',
-    'color': '#373a3c'
+    position: "relative",
+    display: "table",
+    border: "1px solid #ddd",
+    height: "100%",
+    minHeight: "500px",
+    width: "100%",
+    fontFamily:
+      '-apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", Arial, sans-serif',
+    fontSize: "1rem",
+    color: "#373a3c"
   },
   githubHeaderOuter: {
-    display: 'table-row',
-    height: '1px',
+    display: "table-row",
+    height: "1px"
   },
   githubHeader: {
-    display: 'table-cell',
-    top: '1px',
-    left: '1px',
-    padding: '10px',
-    'borderBottom': '1px solid #ddd',
+    display: "table-cell",
+    top: "1px",
+    left: "1px",
+    padding: "5px",
+    borderBottom: "1px solid #ddd"
   },
   githubIconOcticon: {
-    font: 'normal normal 40px octicons',
+    font: "normal normal 40px octicons"
   },
   githubIcon: {
-    display: 'inline',
-    'float': 'left',
-    padding: '9px 0 0',
-    width: '35px',
-    height: '35px',
+    display: "inline",
+    float: "left",
+    padding: "9px 0 0",
+    width: "35px",
+    height: "35px"
   },
   githubProfile: {
-    display: 'inline-block',
-    'float': 'left',
-    margin: '0 auto',
-    padding: '6px 10px 5px',
-    color: '#495961',
-    'fontSize': '20px',
-    'textDecoration': 'none'
+    display: "inline-block",
+    float: "left",
+    margin: "0 auto",
+    padding: "3px 15px 5px",
+    color: "#495961",
+    fontSize: "20px",
+    textDecoration: "none"
   },
   githubFullname: {
-    'fontSize': '26px',
-    'lineHeight': '30px',
+    fontSize: "24px",
+    lineHeight: "30px"
   },
   githubUsername: {
-    'fontSize': '20px',
-    'fontStyle': 'normal',
-    'fontWeight': '300',
-    'lineHeight': '24px',
-    'color': '#666',
+    fontSize: "20px",
+    fontStyle: "normal",
+    fontWeight: "300",
+    lineHeight: "24px",
+    color: "#666"
   },
   githubAvatar: {
-    display: 'inline',
-    'float': 'right',
-    'marginRight': '10px',
-    'paddingRight': '20px',
-    'maxWidth': '60px',
-    'height': '67px',
+    display: "inline",
+    float: "right",
+    marginRight: "10px",
+    paddingRight: "20px",
+    maxWidth: "60px",
+    height: "67px"
   },
   githubAvatarImg: {
-    padding: '3px',
-    height: '100%',
-    border: '1px solid #ddd',
-    'boxShadow': '1px 1px 3px #ccc',
+    padding: "3px",
+    height: "100%",
+    border: "1px solid #ddd",
+    boxShadow: "1px 1px 3px #ccc"
   },
   githubFeedlistOuter: {
-    'height': '100%',
-    'display': 'table-row',
+    height: "100%",
+    display: "table-row"
   },
   githubFeedlist: {
-    'overflowY': 'auto',
-    'position': 'relative',
-    'display': 'table-cell',
+    overflowY: "auto",
+    position: "relative",
+    display: "table-cell"
   },
   githubFeedlistInner: {
-    'position': 'absolute',
-    'width': '100%',
-    'minHeight': '100px',
+    position: "absolute",
+    width: "100%",
+    minHeight: "100px"
   },
   githubEvent: {
-    'borderTop': '1px solid #f1f1f1',
-    padding: '1em 0 0',
-    padding: '1.5em 0 1.5em 25px',
+    borderTop: "1px solid #f1f1f1",
+    padding: "1em 0 0",
+    padding: "1.5em 15px 1.5em 15px"
   },
   githubEventOcticon: {
-    color: '#bbb',
-    'marginRight': '10px',
+    color: "#bbb",
+    marginRight: "10px"
   },
   ghUrl: {
-    'color': '#0275d8',
+    color: "#0275d8"
   },
   githubTitle: {
-    display: 'inline-block',
-    'fontSize': '13px',
+    display: "inline-block",
+    fontSize: "13px"
   },
   githubTime: {
-    display: 'inline-block',
-    'fontSize': '12px',
-    'marginLeft': '3px',
-    color: '#bbb',
+    display: "inline-block",
+    fontSize: "12px",
+    marginLeft: "3px",
+    color: "#bbb"
   },
   githubFooter: {
-    padding: '5px',
-    'fontWeight': 'bold',
-    'fontSize': '13px',
-    'borderTop': '1px solid #ddd',
-    'fontFamily': 'Helvetica, arial, freesans, clean, sans-serif',
+    padding: "5px",
+    fontWeight: "bold",
+    fontSize: "12px",
+    borderTop: "1px solid #ddd",
+    fontFamily: "Helvetica, arial, freesans, clean, sans-serif"
   },
   githubFooterP: {
-    'display': 'inline'
+    display: "inline"
   },
   githubFooterA: {
-    'float': 'right',
-    'color': 'black'
+    float: "right",
+    color: "black"
   }
-}
+};
